e1fe0c98b0c33bedfca6e2bf050b44f4b773feae
tessdja <tessdj@gmail.com>
2025-10-04 07:49:23 -0400

    initialized project and created high-level structure

---
+++ get_lib_versions.py
@@ -0,0 +1,11 @@
+import importlib.metadata
+packages = [
+    "langgraph"
+ 
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
