fc89fadd7b8f8ad56b72fe24c65f6d540135da1a
tessdja <tessdj@gmail.com>
2026-02-05 10:34:10 -0500

    added download report in report service

--- research_and_analyst/api/services/report_service.py
+++ research_and_analyst/api/services/report_service.py
@@ -1,13 +1,20 @@
 import uuid
+import os
+from fastapi.responses import FileResponse
+
 from research_and_analyst.utils.model_loader import ModelLoader
 from research_and_analyst.workflows.report_generator_workflow import AutonomousReportGenerator
 from research_and_analyst.logger import GLOBAL_LOGGER
 from research_and_analyst.exception.custom_exception import ResearchAnalystException
+from langgraph.checkpoint.memory import MemorySaver
+
+_shared_memory = MemorySaver()
 
 class ReportService:
     def __init__(self):
         self.llm = ModelLoader().load_llm()
         self.reporter = AutonomousReportGenerator(self.llm)
+        self.reporter.memory = _shared_memory
         self.graph = self.reporter.build_graph()
         self.logger = GLOBAL_LOGGER.bind(module="ReportService")
 
@@ -28,7 +35,7 @@ class ReportService:
             self.logger.error("Error initiating report generation", error=str(e))
             raise ResearchAnalystException("Failed to start report generation", e)
         
-    def update_feedback(self, thread_id: str, feedback: str):
+    def submit_feedback(self, thread_id: str, feedback: str):
         """Update human feedback in graph state."""
         try:
             thread = {"configurable": {"thread_id": thread_id}}
@@ -41,15 +48,42 @@ class ReportService:
             self.logger.error("Error updating feedback", error=str(e))
             raise ResearchAnalystException("Failed to update feedback", e)
         
+    # def get_report_status(self, thread_id: str):
+    #     """Fetch latest state or final report"""
+    #     try:
+    #         thread = {"configurable": {"thread_id": thread_id}}
+    #         state = self.graph.get_state(thread)
+    #         if not state:
+    #             self.logger.warning("No state found for thread", thread_id=thread_id)
+    #             return {"status": "not_found"}
+
+    #         final_report = state.values.get("final_report")
+    #         if final_report:
+    #             file_docx = self.reporter.save_report(final_report, "AI_Report", "docx")
+    #             file_pdf = self.reporter.save_report(final_report, "AI_Report", "pdf")
+    #             return {
+    #                 "status": "completed",
+    #                 "docx_path": file_docx,
+    #                 "pdf_path": file_pdf,
+    #             }
+    #         return {"status": "in_progress"}
+    #     except Exception as e:
+    #         self.logger.error("Error fetching report status", error=str(e))
+    #         raise ResearchAnalystException("Failed to fetch report status", e)
+
+
     def get_report_status(self, thread_id: str):
-        """Fetch latest state or final report"""
+        """Fetch latest state or final report."""
         try:
             thread = {"configurable": {"thread_id": thread_id}}
             state = self.graph.get_state(thread)
             final_report = state.values.get("final_report")
+            topic = state.values.get("topic", "AI_Report")
+
             if final_report:
-                file_docx = self.reporter.save_report(final_report, "AI_Report", "docx")
-                file_pdf = self.reporter.save_report(final_report, "AI_Report", "pdf")
+                # now topic-based report folder name
+                file_docx = self.reporter.save_report(final_report, topic, "docx")
+                file_pdf = self.reporter.save_report(final_report, topic, "pdf")
                 return {
                     "status": "completed",
                     "docx_path": file_docx,
@@ -59,3 +93,18 @@ class ReportService:
         except Exception as e:
             self.logger.error("Error fetching report status", error=str(e))
             raise ResearchAnalystException("Failed to fetch report status", e)
+
+    @staticmethod
+    def download_file(file_name: str):
+        """Download generated report."""
+        report_dir = os.path.join(os.getcwd(), "generated_report")
+        for root, _, files in os.walk(report_dir):
+            if file_name in files:
+                return FileResponse(
+                    path=os.path.join(root, file_name),
+                    filename = file_name,
+                    media_type= "application/octet-stream"
+                )
+        return{"error": f"File {file_name} not found"}
+
+
